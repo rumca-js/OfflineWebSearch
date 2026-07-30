708da7c8f8ed51249b776aba2bb2843f79833841
Rumpel Stilskin <rozbujnik@gmial.com>
2026-07-30 22:54:09 +0200

    .db files can be selected by the local file picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
