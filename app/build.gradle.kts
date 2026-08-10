46e9917c940742e6b605563700667ac05c04b454
Rumpel Stilskin <rozbujnik@gmial.com>
2026-08-10 21:06:30 +0200

    Added ConfigurationEntry and SearchViewEntry models. Database properties are read from SQLite files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.11"
+        versionCode = 14
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
