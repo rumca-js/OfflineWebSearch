f347d4bba4601b5b45ae9ae2085293bf32b35bf4
Rumpel Stilskin <rozbujnik@gmial.com>
2026-08-18 22:24:38 +0200

    Preparing for new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.0.14"
+        versionCode = 17
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
