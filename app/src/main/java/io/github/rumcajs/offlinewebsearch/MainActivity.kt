6cb84951c0b193bf051fefba80ab636f808aa6a2
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-05 19:54:43 +0200

    Search contains correct icon now. Entry detail text should be copyable, and should highlight links like https

--- app/src/main/java/io/github/rumcajs/offlinewebsearch/MainActivity.kt
+++ app/src/main/java/io/github/rumcajs/offlinewebsearch/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
