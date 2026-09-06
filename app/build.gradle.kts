6aaaa2d76b31676f54ddb424320dbede3f8130e2
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-06 21:20:32 +0200

    Copying age designation from source

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.0.24"
+        versionCode = 27
+        versionName = "1.0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
