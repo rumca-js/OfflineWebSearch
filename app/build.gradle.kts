f4544ad564160a85f958e7fc73bc4d856f26ec2b
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-11 21:09:50 +0200

    Updated model table

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.0.26"
+        versionCode = 29
+        versionName = "1.0.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
