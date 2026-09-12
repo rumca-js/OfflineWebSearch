787861824a11f619be9f5f5f5921ae57f768a523
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-12 14:46:16 +0200

    Tags are edited through a small button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.0.28"
+        versionCode = 31
+        versionName = "1.0.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
