ab50402f38db036669cb07468e8cdd50c9d6acfb
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-16 22:29:27 +0200

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "io.github.rumcajs.offlinewebsearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.0.30"
+        versionCode = 33
+        versionName = "1.0.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
