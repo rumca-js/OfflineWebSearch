feb35b42567a2b0c786701f51e86374546c8c4f6
Rumpel Stilskin <rozbujnik@gmial.com>
2026-09-16 19:05:03 +0200

    Fixed unit test

--- app/src/test/java/io/github/rumcajs/offlinewebsearch/util/UrlLocationTest.kt
+++ app/src/test/java/io/github/rumcajs/offlinewebsearch/util/UrlLocationTest.kt
@@ -119,10 +119,14 @@ class UrlLocationTest {
 
     @Test
     fun testYouTubeRedirectTest() {
-        val customGoogleUrl = "https://www.youtube.com/redirect?event=video_description&redir_token=QUZZTVljRXRSdUJkRE5vMVJYMHZPcVZrTzhNUHxBTl9pYzRlM0Z6UnNZRHZDRGRjVlZuSkM3V2RvdDBHSHJqVFVxWHVYTG9HR3BPZHdkM1doaG41aERtMTg3NXozdFlNamhMSURjSzZENjlqd2xPeEpsbUJLbW5wRjROeURPRXl3&q=https%3A%2F%2Fmeter.com%2Fltt&v=3xngArcFpek"
+        val youtubeUrl = "https://www.youtube.com/redirect?event=video_description&redir_token=QUZZTVljRXRSdUJkRE5vMVJYMHZPcVZrTzhNUHxBTl9pYzRlM0Z6UnNZRHZDRGRjVlZuSkM3V2RvdDBHSHJqVFVxWHVYTG9HR3BPZHdkM1doaG41aERtMTg3NXozdFlNamhMSURjSzZENjlqd2xPeEpsbUJLbW5wRjROeURPRXl3&q=https%3A%2F%2Fmeter.com%2Fltt&v=3xngArcFpek"
         assertEquals(
             "https://meter.com/ltt",
-            UrlLocation.getGoogleRedirectFix(customGoogleUrl, domainLocation = "custom_redirect")
+            UrlLocation.getYoutubeRedirectFix(youtubeUrl)
+        )
+        assertEquals(
+            "https://meter.com/ltt",
+            UrlLocation(youtubeUrl).getYoutubeRedirectFix()
         )
     }
 
@@ -130,9 +134,7 @@ class UrlLocationTest {
     fun testGetUrlArgAndCleaned() {
         val testUrl = "https://example.com/path?key1=hello+world&key2=foo%2Fbar#frag"
         assertEquals("hello world", UrlLocation.getUrlArg(testUrl, "key1"))
-        assertEquals("https://hello world", UrlLocation.getCleanedLink(UrlLocation.getUrlArg(testUrl, "key1")!!))
         assertEquals("foo/bar", UrlLocation.getUrlArg(testUrl, "key2"))
-        assertEquals("https://foo/bar", UrlLocation.getCleanedLink(UrlLocation.getUrlArg(testUrl, "key2")!!))
         assertEquals(null, UrlLocation.getUrlArg(testUrl, "nonexistent"))
     }
 }
